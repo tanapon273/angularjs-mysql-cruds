17c9131ca5c2a2d8167e0cad58643f5e5e365df1
tanapon273 <limitatr00t@rmutsvmail.com>
2016-12-13 01:22:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php  error_reporting(E_ALL & ~E_NOTICE);
  $db_name = "anjstest";  
  $mysql_user = "root";  
- $mysql_pass = "12345678";  
+ $mysql_pass = "";  
  $server_name = "localhost";
  $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
 mysqli_set_charset($con,"utf8");
